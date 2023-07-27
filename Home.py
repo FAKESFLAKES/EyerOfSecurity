054c2d3d0b27d05a5d0ac073a1be7d0bf1aa9c8d
FAKESFLAKES <133153614+FAKESFLAKES@users.noreply.github.com>
2023-07-27 17:34:37 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,18 @@
-teste
+from colorama import init, Fore, Style
+
+# Inicializar a biblioteca Colorama
+init()
+
+def home():
+    print(f"{Fore.RED}Eyer Of Security Group{Style.RESET_ALL}\n")
+    print(f"{Fore.GREEN}Esse script foi feito para fins de estudos e educativos{Style.RESET_ALL}\n")
+    print("Opções:")
+    print("1 - Opção 1")
+    print("2 - Opção 2")
+    print("3 - Opção 3")
+    print("4 - Opção 4")
+    print("5 - Opção 5")
+    print("0 - Sair do script.")
+
+if __name__ == "__main__":
+    home()
